814cb5b16d3b68a7b5fa7e64493b8b8dc0e37618
Miroslava Sotakova <mirka@google.com>
2021-12-13 04:23:13 +0000

    day 12

---
+++ advent_12b.py
@@ -0,0 +1,61 @@
+import copy
+from queue import Queue
+
+
+with open("advent_12.txt") as f:
+  lines = [line.strip() for line in f.readlines()]
+
+
+class Cave(object):
+  def __init__(self, name):
+    self.name = name
+    self.neighbors = set()
+
+  def __str__(self):
+    return "{}: {}".format(self.name, self.neighbors)
+
+
+caves = dict()
+
+
+for line in lines:
+  line_c = set(line.split("-"))
+  for c in line_c:
+    if c not in caves:
+      caves[c] = Cave(c)
+      caves[c].neighbors = line_c.difference(set([c]))
+    else:
+      caves[c].neighbors.add(line_c.difference(set([c])).pop())
+
+
+print([cave.__str__() for cave in caves.values()])
+
+
+class Visited(object):
+  def __init__(self, cave, visited, small_twice):
+    self.cave = cave
+    self.visited = visited
+    self.small_twice = small_twice
+
+
+paths = []
+q = Queue(maxsize=0)
+
+q.put(Visited(caves["start"], ["start"], False))
+
+while not q.empty():
+  current = q.get()
+  for n in current.cave.neighbors:
+    if n == "end":
+      paths.append(current.visited + ["end"])
+    else:
+      small_twice = current.small_twice
+      cont = n.isupper() or n not in current.visited
+      if not cont and n != "start" and not small_twice:
+        small_twice = True
+        cont = True
+      if cont:
+        new_visited = copy.deepcopy(current.visited) + [n]
+        q.put(Visited(caves[n], new_visited, small_twice))
+
+print(len(paths))
